4d72be31305f68d3427a9dd0bb534acfa911fb60
lijinhuai <lijinhuai@vip.qq.com>
2017-07-06 21:42:20 +0800

    初次提交

---
+++ demo1/runoob1.js
@@ -0,0 +1,9 @@
+// document.write("It works!");
+
+// document.write(require("./runoob2.js"));
+
+// require("!style-loader!css-loader!./style.css");
+// document.write(require("./runoob2.js"));
+
+require("./style.css");
+document.write(require("./runoob2.js"));
